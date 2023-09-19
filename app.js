fcdf57abcb18e70d7e343cf7c4c790cef5f392bf
tb0ied <pchonacki@proton.me>
2023-09-19 11:35:09 +0100

    udpate

---
+++ app.js
@@ -0,0 +1,9 @@
+let username = prompt("Is the patient awake?");
+//console.log("The users name is " + username);
+
+let eyes = prompt("Is the patient breathing?");
+//console.log(eyes);
+
+//alert(
+//"Hello there " + username + ". You have beautiful " + eyes + " eyes."
+// );
